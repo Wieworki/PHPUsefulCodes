d29c07a3c3a1c35af5fdf44dbf3779ca4293a57d
Augusto Britez <45775681+Wieworki@users.noreply.github.com>
2022-07-28 16:49:01 -0300

    Create credenciales.php

---
+++ conexionDB/credenciales.php
@@ -0,0 +1,8 @@
+<?php
+    $host = '127.0.0.1';            //Host de la base de datos
+    $port = 3306;                   //Numero de puerto de la base de datos, usado para la conexion
+    $dbname = 'base1';              //Nombre de la base de datos
+    $user = 'root';                 //Nombre de usuario de la base de datos
+    $pass = '';                     //Contraseña del usuario de la base de datos
+    $charset = 'utf8mb4';           //Cotejamiento usado
+?>
